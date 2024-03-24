3e03bbc07449daab623789e21284a6103f2c715c
teodoromatheus <matheusteodoro.r@gmail.com>
2024-03-24 10:52:17 -0300

    feat: Loop infinito para manter processo

--- src/exercicios/02.tratamento_desafio.py
+++ src/exercicios/02.tratamento_desafio.py
@@ -13,36 +13,38 @@ def calculo_bonus()->str:
     '''
     Função que retorna o nome do usuário (input), salário (calculated) e bônus (input)
     '''
-    nome_usuario = str(input("Digite seu nome: "))
-    if nome_usuario.isdigit():
-        print("O nome é um número")
-        exit()
-    elif nome_usuario.isspace():
-        print("Nome está em branco")
-        exit()
-    else:
-        pass
-    
-    try:
-        salario = float(input("Digite o seu salário: "))
-        if salario <= 0:
-            print("Valor do salário inválido")
-            exit()
+    NOME_GATE = False
+    SALARIO_GATE = False
+    BONUS_GATE = False
+
+    while NOME_GATE is not True:
+        nome_usuario = str(input("Digite seu nome: "))
+        if nome_usuario.isdigit():
+            print("O nome é um número")
+        elif nome_usuario.isspace():
+            print("Nome está em branco")
         else:
-            pass
-    except ValueError:
-        print("Não é um número válido")
-        exit()
+            NOME_GATE = True
+
+    while SALARIO_GATE is not True:
+        try:
+            salario = float(input("Digite o seu salário: "))
+            if salario <= 0:
+                print("Valor do salário negativo ou zerado")
+            else:
+                SALARIO_GATE = True
+        except ValueError:
+            print("Não é um número válido")
     
-    try:
-        bonus = float(input("Digite o seu bônus (ex.: '10' para 10%): "))
-        if bonus < 0:
-            print("Valor do bônus menor que 0")
-            exit()
-        else:
-            pass
-    except ValueError:
-        print("Não é um número válido")
+    while BONUS_GATE is not True:
+        try:
+            bonus = float(input("Digite o seu bônus (ex.: '10' para 10%): "))
+            if bonus < 0:
+                print("Valor do bônus menor que 0")
+            else:
+                BONUS_GATE = True
+        except ValueError:
+            print("Não é um número válido")
     
     bonus_calculado = round(salario*bonus/100,1)
     return print(f"{nome_usuario}, seu salário é de {salario} reais e seu bônus de {bonus_calculado} reais")
